d3c5bc6bd3eca4691ba1636d4d6b113ba0f68557
Jerome Riga <jriga@zemis.co.uk>
2024-11-27 23:42:37 +0000

    Add org session for ask

--- ask-org.el
+++ ask-org.el
@@ -8,9 +8,45 @@
 ;;; Options
 (require 'ask-core)
 
-(defun ask-project-client ()
+(defvar ask-clients-reg (make-hash-table))
+
+(defun ask-org-project-properties ()
+  "Extract the properties of the current org file."
+  (save-excursion
+    (goto-char (point-min))
+    (search-forward "#+properties:" nil t)
+    (let* ((line (buffer-substring-no-properties (line-beginning-position) (line-end-position) ))
+           (args (string-replace "#+properties: " "" line)))
+      (mapcar (lambda (x) (if (string-match ":" x) (intern x) x))
+              (split-string args " ")))))
+
+(defun ask-rand-session-name ()
+  "Return a random string."
+  (format "%s" (time-convert (current-time) 'integer)))
+
+(defun ask-add-session-property (session)
+  (save-excursion
+    (goto-char (point-min))
+    (search-forward "#+properties:" nil t)
+    (end-of-line)
+    (insert (format " :session %s" session))))
+
+(defun ask-org-client-add (options)
+  ""
+  (let ((session (or (plist-get options :session)
+                     (ask-rand-session-name)))
+        (client (apply 'ask-make-client `(nil nil options))))
+    (puthash session client ask-clients-reg)
+    (ask-add-session-property session)
+    client))
+
+(defun ask-org-client ()
   "Prompt for the project client information."
-  (ask-make-client))
+  (let* ((options (ask-org-project-properties))
+         (session (plist-get options :session))
+         (client (gethash session ask-clients-reg nil)))
+    (if client client (ask-org-client-add options))))
+
 
 (defun ask-prompt-at-point ()
   (interactive)
@@ -30,7 +66,7 @@
           (progn
             (re-search-forward "^#\\+end_prompt" nil t)
             (insert "\n\n")
-            (insert (apply 'ask-prompt (ask-project-client)
+            (insert (apply 'ask-prompt (ask-org-client)
                            (append '() `(,(ask-prompt-with-org-format prompt)) plist))))
         (message "No valid prompt found at point.")))))
 
