cd718969799322a66e176bb55e2525ab4bd7044c
Jerome Riga <jriga@zemis.co.uk>
2024-11-29 23:56:39 +0000

    Send attachment
    `ask-mk-msg` dispatches based on presence of attachments to
    `ask-mk-single-msg` or `ask-mk-multi-msg`
    
    Need to present the uploaded image before the prompt otherwise the llm
    does not known about the images

--- ask-org.el
+++ ask-org.el
@@ -20,16 +20,6 @@
 (defvar ask-clients-reg (make-hash-table :test 'equal))
 (defvar ask-org-properties-token "#+properties:")
 
-;; (defun ask-org-project-properties ()
-;;   "Extract the properties of the current org file."
-;;   (save-excursion
-;;     (goto-char (point-min))
-;;     (search-forward ask-org-properties-token nil t)
-;;     (let* ((line (buffer-substring-no-properties (line-beginning-position) (line-end-position) ))
-;;            (str-to-replace (format "%s " ask-org-properties-token))
-;;            (args (string-replace str-to-replace "" line)))
-;;       (mapcar (lambda (x) (if (string-match ":" x) (intern x) x))
-;;               (split-string args " ")))))
 
 (defun ask-org-project-properties ()
   "Extract the properties of the current org file."
@@ -54,10 +44,10 @@
           nil
         (progn
           (end-of-line)
-          (insert token))))))
+          (insert (concat " " token)))))))
 
 (defun ask-org-client-add (options)
-  ""
+  "Add a newly created client with OPTIONS to the client registry."
   (let* ((session (or (plist-get options :session)
                      (ask-rand-session-name)))
          (client-options (cons '() (cons '() options)))
@@ -73,11 +63,35 @@
          (client (gethash session ask-clients-reg)))
     (if client client (ask-org-client-add options))))
 
+
+(defun ask-org-parse-clean-string (input)
+  "Strip unnecessary tokens from INPUT string."
+  (string-trim (string-replace "[[]]" "" input)))
+
+(defun ask-org-parse-raw-prompt (input)
+  "Parse a raw input string, extracting attachments and cleaning the prompt.
+INPUT is the raw input string containing potential file links."
+  (let* ((attachments '())
+         (cleaned-prompt
+          (replace-regexp-in-string
+           "\\[\\[\\([^]]+\\)\\]\\]"
+           (lambda (match)
+             (let ((file-path (match-string 1 match)))
+               (push (string-replace "file:" "" file-path) attachments)
+               ""))
+           input nil nil 1)))
+    (if attachments
+       (list
+        :prompt (ask-org-parse-clean-string cleaned-prompt)
+        :attachments (nreverse attachments))
+       input)))
+
+
 (defun ask-prompt-at-point ()
   "Send prompt at point."
   (interactive)
   (save-excursion
-    (let ((plist  (when (re-search-backward "^#\\+begin_prompt" nil t)
+    (let ((request-options  (when (re-search-backward "^#\\+begin_prompt" nil t)
                     (forward-word)
                     (forward-word)
                     (read (concat "(" (buffer-substring-no-properties (point) (line-end-position)) ")")))) ;; reads properties
@@ -93,7 +107,7 @@
             (re-search-forward "^#\\+end_prompt" nil t)
             (insert "\n\n")
             (insert (apply 'ask-prompt (ask-org-client)
-                           (append '() `(,(ask-prompt-with-org-format prompt)) plist))))
+                           (append '() `(,(ask-org-parse-raw-prompt (ask-prompt-with-org-format prompt))) request-options))))
         (message "No valid prompt found at point.")))))
 
 (defun ask-dump-client ()
