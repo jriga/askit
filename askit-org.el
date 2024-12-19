aa7e59711470de544edb55554953db40795b912f
Jerome Riga <jriga@zemis.co.uk>
2024-12-19 09:52:43 +0000

    Rename package from ask to askit

--- askit-org.el
+++ askit-org.el
@@ -1,4 +1,4 @@
-;;; ask-org.el --- ask org integration    -*- lexical-binding: t; -*-
+;;; askit-org.el --- ask org integration    -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2024 Jerome Riga
 
@@ -12,7 +12,7 @@
 
 ;;; Code:
 
-(require 'ask-core)
+(require 'askit-core)
 (require 'tempo)
 (require 'org-tempo)
 
@@ -20,32 +20,32 @@
 (setq org-structure-template-alist
       (append org-structure-template-alist '(("p" . "prompt"))))
 
-;; `<pr` expands to #+properties: add ask-properties for org mode
+;; `<pr` expands to #+properties: add askit-properties for org mode
 (setq org-tempo-keywords-alist
       (append org-tempo-keywords-alist '(("pr" . "properties"))))
 
 
-(defvar ask-clients-reg (make-hash-table :test 'equal))
-(defvar ask-org-properties-token "#+properties:")
+(defvar askit-clients-reg (make-hash-table :test 'equal))
+(defvar askit-org-properties-token "#+properties:")
 
 
-(defun ask-org-project-properties ()
+(defun askit-org-project-properties ()
   "Extract the properties of the current org file."
   (save-excursion
     (goto-char (point-min))
-    (search-forward ask-org-properties-token nil t)
+    (search-forward askit-org-properties-token nil t)
     (let ((line (buffer-substring-no-properties (point) (line-end-position))))
       (read (concat "(" line ")")))))
 
-(defun ask-rand-session-name ()
+(defun askit-rand-session-name ()
   "Return a random string."
   (format "%s" (time-convert (current-time) 'integer)))
 
-(defun ask-add-property (key value)
+(defun askit-add-property (key value)
   "Add a property `KEY` and its value `VALUE` to properties list."
   (save-excursion
     (goto-char (point-min))
-    (search-forward ask-org-properties-token nil t)
+    (search-forward askit-org-properties-token nil t)
     (let ((line (thing-at-point 'line))
           (token (format "%s %s" key value)))
       (if (string-match token line)
@@ -54,29 +54,29 @@
           (end-of-line)
           (insert (concat " " token)))))))
 
-(defun ask-org-client-add (options)
+(defun askit-org-client-add (options)
   "Add a newly created client with OPTIONS to the client registry."
   (let* ((session (or (plist-get options :session)
-                     (ask-rand-session-name)))
+                     (askit-rand-session-name)))
          (client-options (cons '() (cons '() options)))
-         (client (apply 'ask-make-client client-options)))
-    (puthash session client ask-clients-reg)
-    (ask-add-property :session session)
+         (client (apply 'askit-make-client client-options)))
+    (puthash session client askit-clients-reg)
+    (askit-add-property :session session)
     client))
 
-(defun ask-org-client ()
+(defun askit-org-client ()
   "Prompt for the project client information."
-  (let* ((options (ask-org-project-properties))
+  (let* ((options (askit-org-project-properties))
          (session (plist-get options :session))
-         (client (gethash session ask-clients-reg)))
-    (if client client (ask-org-client-add options))))
+         (client (gethash session askit-clients-reg)))
+    (if client client (askit-org-client-add options))))
 
 
-(defun ask-org-parse-clean-string (input)
+(defun askit-org-parse-clean-string (input)
   "Strip unnecessary tokens from INPUT string."
   (string-trim (string-replace "[[]]" "" input)))
 
-(defun ask-org-parse-raw-prompt (input)
+(defun askit-org-parse-raw-prompt (input)
   "Parse a raw input string, extracting attachments and cleaning the prompt.
 INPUT is the raw input string containing potential file links."
   (let* ((attachments '())
@@ -90,12 +90,12 @@ INPUT is the raw input string containing potential file links."
            input nil nil 1)))
     (if attachments
        (list
-        :prompt (ask-org-parse-clean-string cleaned-prompt)
+        :prompt (askit-org-parse-clean-string cleaned-prompt)
         :attachments (nreverse attachments))
        input)))
 
 
-(defun ask-prompt-at-point ()
+(defun askit-prompt-at-point ()
   "Send prompt at point."
   (interactive)
   (save-excursion
@@ -114,28 +114,28 @@ INPUT is the raw input string containing potential file links."
           (progn
             (re-search-forward "^#\\+end_prompt" nil t)
             (insert "\n\n")
-            (insert (apply 'ask-prompt (ask-org-client)
-                           (append '() `(,(ask-org-parse-raw-prompt (ask-prompt-with-org-format prompt))) request-options))))
+            (insert (apply 'askit-prompt (askit-org-client)
+                           (append '() `(,(askit-org-parse-raw-prompt (askit-prompt-with-org-format prompt))) request-options))))
         (message "No valid prompt found at point.")))))
 
-(defun ask-dump-client ()
+(defun askit-dump-client ()
   "Dump the current buffer session."
   (interactive)
-  (let* ((options (ask-org-project-properties))
-         (client (ask-org-client))
-         (link (ask-org-roam-save-session (plist-get options :session)
-                             (ask-history client)
-                             (ask-usage client)
-                             (or (plist-get options :model) ask-model)
-                             (or (plist-get options :version) ask-version)))))
-  (ask-add-property :link link))
+  (let* ((options (askit-org-project-properties))
+         (client (askit-org-client))
+         (link (askit-org-roam-save-session (plist-get options :session)
+                             (askit-history client)
+                             (askit-usage client)
+                             (or (plist-get options :model) askit-model)
+                             (or (plist-get options :version) askit-version)))))
+  (askit-add-property :link link))
 
-(defun ask-restore-client ()
+(defun askit-restore-client ()
   "Restore the current buffer session."
   (interactive)
-  (let* ((options (ask-org-project-properties))
-         (client (ask-load-saved-file (plist-get options :link))))
-    (puthash (plist-get options :session) client ask-clients-reg)))
+  (let* ((options (askit-org-project-properties))
+         (client (askit-load-saved-file (plist-get options :link))))
+    (puthash (plist-get options :session) client askit-clients-reg)))
 
-(provide 'ask-org)
-;;; ask-org.el ends here
+(provide 'askit-org)
+;;; askit-org.el ends here
--- askit-prog.el
+++ askit-prog.el
@@ -1,4 +1,4 @@
-;;; ask-prog.el --- Ask coding utilities    -*- lexical-binding: t; -*-
+;;; askit-prog.el --- Ask coding utilities    -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2024 Jerome Riga
 
@@ -12,41 +12,41 @@
 
 ;;; Code:
 
-(require 'ask-core)
+(require 'askit-core)
 
-(defun ask-prog-client ()
+(defun askit-prog-client ()
   "Prompt for the project client information."
-  (ask-make-client))
+  (askit-make-client))
 
-(defun ask-prompt-with-org-format (p)
+(defun askit-prompt-with-org-format (p)
   "Format prompt P for org mode."
   (concat p "
 Only do the following if your response contains some code snippets.
 Enclose the code portion of your response with the tag #+begin_src #+end_src for org-mode.
 Don't forget to put the code language after the #+begin_src."))
 
-(setq ask-template-prompt "Review the following code, focus on clarity and idiomatic code form
+(setq askit-template-prompt "Review the following code, focus on clarity and idiomatic code form
 provide code suggestions. Each suggestion have a one line short description.
 ")
 
-(defun ask-code-review-prompt (code)
+(defun askit-code-review-prompt (code)
   "Prompt for a code review on the given CODE."
-  (ask-prompt-with-org-format
+  (askit-prompt-with-org-format
    (format "%s\n\nThe tag <code> is used to show the code to review.\n<code>%s</code>"
-           ask-template-prompt
+           askit-template-prompt
            code)))
 
-(defun ask-code-system-prompt (m)
+(defun askit-code-system-prompt (m)
   "Prompt M for the system information."
   (format "You are an expert %s programmer" m))
 
 
-(defun ask-code-review ()
+(defun askit-code-review ()
   "Review code in current buffer."
   (interactive)
-  (ask-code-review-region (point-min) (point-max)))
+  (askit-code-review-region (point-min) (point-max)))
 
-(defun ask-code-review-region (&optional rbe ren)
+(defun askit-code-review-region (&optional rbe ren)
   "Review code in region between RBE and REN."
   (interactive)
   (let* ((rb (or rbe (region-beginning)))
@@ -58,13 +58,13 @@ provide code suggestions. Each suggestion have a one line short description.
     (when (zerop (buffer-size))
       (insert "#+TITLE: AskIT\n#+PROPERTY: header-args:elisp :results pp\n"))
     (goto-char (point-max))
-    (insert (format "\n\n#+begin_prompt\n%s\n#+end_prompt\n#+property: :begin_region %s :end_region %s\n\n" ask-template-prompt rb re))
+    (insert (format "\n\n#+begin_prompt\n%s\n#+end_prompt\n#+property: :begin_region %s :end_region %s\n\n" askit-template-prompt rb re))
     ;;; TODO: (insert (org-insert-link link "source file"))
-    (insert (ask-prompt (ask-prog-client)
-                        (ask-code-review-prompt current-region)
-                        :system (ask-code-system-prompt buffer-major-mode)))
+    (insert (askit-prompt (askit-prog-client)
+                        (askit-code-review-prompt current-region)
+                        :system (askit-code-system-prompt buffer-major-mode)))
     (org-mode)))
 
 
-(provide 'ask-prog)
-;;; ask-prog.el ends here
+(provide 'askit-prog)
+;;; askit-prog.el ends here
